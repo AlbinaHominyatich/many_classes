a8fca9aab7ba251d6e10c61122943befa442749c
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-05-09 18:51:30 +0300

    додали клас Student та вивели його по імені в методі admit_student

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ class School:
         self.students = students #список студентів
     def admit_student(self, student): #зарахування студентів
         self.students.append(student)
-        print(f'{}') #дописати, коли створимо клас студентів
+        print(f'{student.name} був доданий до школи {self.name}') #дописати, коли створимо клас студентів
 
 class Student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
